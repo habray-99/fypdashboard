1cab1352833e81d751d3e45e424be19e0cf38ae0
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-14 08:35:03 +0545

    Add GetGymMembersRepo class to handle gym member data retrieval and addition

---
+++ lib/repo/gym_repo.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:fypdashboard/models/gym_member.dart';
+import 'package:http/http.dart' as http;
+
+import '../utils/apis.dart';
+
+class GetGymMembersRepo {
+  static Future<void> getGymMembers({
+    required String gymId,
+    required Function(List<GymMember> exerciseRecommendation) onSuccess,
+    required Function(String message) onError,
+  }) async {
+    try {
+      var header = {
+        "Accept": "application/json",
+      };
+
+      // Construct the URL with query parameters
+      var url = Uri.parse(Apis.fetchAllMemberByGym).replace(queryParameters: {
+        'gym_id': gymId,
+      });
+
+      // log(url.toString());
+
+      var response = await http.get(
+        url,
+        headers: header,
+      );
+      // var response = await HttpRequest.get(url, headers: header);
+      // print("Response: ");
+      // log(response.toString());
+      dynamic data = jsonDecode(response.body);
+      if (response.statusCode >= 200 &&
+          data['message'] == "Members fetched successfully") {
+        List<GymMember> recommendations = gymMembersFromJson(data["members"]);
+        onSuccess(recommendations);
+      } else {
+        onError(data["message"].toString());
+      }
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+      onError("Something went wrong.");
+    }
+  }
+
+  static Future<void> addGymMember(
+      {required GymMember member,
+      required Function(dynamic message) onSuccess,
+      required Function(dynamic message) onError}) async {
+    var header = {
+      "Accept": "application/json",
+    };
+
+    var body = member.toJson();
+
+    var response =
+        await http.post(Uri.parse(Apis.baseUrl), headers: header, body: body);
+
+    dynamic data = jsonDecode(response.body);
+    if (data["status"] == "success") {
+      onSuccess(data["message"]);
+    } else {
+      onError(data["message"]);
+    }
+  }
+}
